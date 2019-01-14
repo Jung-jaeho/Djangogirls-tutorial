6baa8a2090f9dc2062526939bbabf79925de7d92
Jung-jaeho <wjdwogh1115@daum.net>
2019-01-15 01:26:52 +0900

    djangogirls-tutorial

---
+++ blog/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import (render,redirect,get_object_or_404)
+from django.contrib.auth.decorators import login_required
+from blog.models import Post
+from django.utils import timezone
+from blog.forms import PostForm
+
+def post_list(request):
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+
+#로그인이 안되어 있을 경우 로그인을 먼저 하도록 해줌
+@login_required(login_url='admin:login')
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+#model form
+@login_required(login_url='admin:login')
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
